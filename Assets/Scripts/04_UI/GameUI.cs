88bea3f66dae7bae4fe0df63431d2031b7386e6c
AgathaYi <tjsfid1030@naver.com>
2025-05-09 11:42:55 +0900

    feat : 점수 문자열 변환 기능 추가 #1
    GameUI

--- Assets/Scripts/04_UI/GameUI.cs
+++ Assets/Scripts/04_UI/GameUI.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameUI : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("CurrentScore")]
+    public TextMeshProUGUI currentScoreText;
+    public TextMeshProUGUI highScoreText;
+
+    [Header("Hp")]
+    public TextMeshProUGUI hpText;
+
+    public void UpdateScoreText(int currentScore, int highScore)
     {
-        
+        // TODO: 게임매니져에서 현재점수, 최고점수를 문자열로 바꿔서 화면에 표시
+        currentScoreText.text = currentScore.ToString();
+        highScoreText.text = highScore.ToString();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
