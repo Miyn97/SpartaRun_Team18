7b5e2ee550a3b1cfce29b0aa4a54f08dbcb61694
minchangyun <yunchang0101@gmail.com>
2025-05-11 00:18:13 +0900

    comment : 오류난 부분 주석처리 #19
    오류난 부분 주석처리

--- Assets/Scripts/01_Models/ObstacleModel.cs
+++ Assets/Scripts/01_Models/ObstacleModel.cs
@@ -96,16 +96,16 @@ public class ObstacleModel : MonoBehaviour
                                                                         // "PlayerModel.cs" 컴포넌트를 받아서 player 변수에 저장
 
 
-        if (player == true)                                             // 부딪친 게임 오브젝트의 태그가 플레이어라면
+/*        if (player == true)*/                                             // 부딪친 게임 오브젝트의 태그가 플레이어라면
         {
             Debug.Log("플레이어와 충돌");                               // 충돌 메서드 작동 확인용 메시지
 
 
             if (player != null)                                         // 예외 처리 (부딪친 게임 오브젝트(플레이어)에 PlayerModel 스크립트가 있는지 확인)
             {
-                float originalSpeed = player.speed;                     // 맞은 시점의 속도를 저장하는 변수 
-                player.hp -= damage;                                    // hp 감소
-                StartCoroutine(SpeedDown(player, originalSpeed));       // SpeedDown 코루틴 호출
+                //float originalSpeed = player.speed;                     // 맞은 시점의 속도를 저장하는 변수 
+                //player.hp -= damage;                                    // hp 감소
+/*                StartCoroutine(SpeedDown(player, originalSpeed)); */      // SpeedDown 코루틴 호출
             }
         }
     }
