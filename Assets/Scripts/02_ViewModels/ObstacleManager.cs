a054518077f7926c776bd01b2a73668b150fb82b
Sage-Teshi <dua2670@gmail.com>
2025-05-12 10:14:20 +0900

    feat: Obstacle 매니저 구현 중
    - Obstacle 매니저 만들기

--- Assets/Scripts/02_ViewModels/ObstacleManager.cs
+++ Assets/Scripts/02_ViewModels/ObstacleManager.cs
@@ -4,10 +4,20 @@ using UnityEngine;
 
 public class ObstacleManager : MonoBehaviour
 {
+    // 각각의 장애물에 대응하는 프리팹을 넣어둘 변수들 (Inspector에서 직접 연결)
+    public GameObject redLinePrefab;            // RedLineTrap 프리팹
+    public GameObject syntaxErrorBoxPrefab;     // SyntaxErrorBox 프리팹
+    public GameObject compileErrorWallPrefab;   // CompileErrorWall 프리팹
+
+    public Transform spawnPoint;                // 장애물이 생성될 위치를 담을 변수
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        SpawnRandomObstacle();  // 장애물 생성 메서드를 호출
     }
 
     // Update is called once per frame
@@ -16,31 +26,16 @@ public class ObstacleManager : MonoBehaviour
         
     }
 
-    public void OnTriggerEnter2D(Collider2D collision)                  // 장애물이 다른 게임 오브젝트와 충돌했을 때
+    void SpawnRandomObstacle()  // 장애물 생성 메서드
     {
-        PlayerModel player = collision.GetComponent<PlayerModel>();     // 부딪친 게임 오브젝트(여기서는 플레이어)의 컴포넌트 중
-                                                                        // "PlayerModel.cs" 컴포넌트를 받아서 player 변수에 저장
+        ObstacleType type = GetRandomObstacleType();    // 랜덤으로 장애물 타입을 선택
 
+        ObstacleModel model = new ObstacleModel(type);  // 장애물 객체 생성
 
-        /*        if (player == true)*/                                             // 부딪친 게임 오브젝트의 태그가 플레이어라면
-        {
-            Debug.Log("플레이어와 충돌");                               // 충돌 메서드 작동 확인용 메시지
+        GameObject prefab = GetPrefabByType(type);      // 장애물 종류에 맞는 프리팹 가져오기
 
-
-            if (player != null)                                         // 예외 처리 (부딪친 게임 오브젝트(플레이어)에 PlayerModel 스크립트가 있는지 확인)
-            {
-                //float originalSpeed = player.speed;                     // 맞은 시점의 속도를 저장하는 변수 
-                //player.hp -= damage;                                    // hp 감소
-                /*                StartCoroutine(SpeedDown(player, originalSpeed)); */      // SpeedDown 코루틴 호출
-            }
-        }
+        Instantiate(prefab, spawnPoint.position, Quaternion.identity);  // 실제로 장애물 오브젝트를 생성
     }
 
-    //IEnumerator SpeedDown(PlayerModel player, float originalSpeed)     // 장애물에 부딪쳤을 떄 잠시동안 속도를 감소 시키는 코루틴 함수 
-    //{                                                                  // 속도가 감소 됐다가 서서히 원래 속도로 돌아온다.
-    //    Debug.Log("속도 감소 코루틴");                                 // 속도 감소 메서드 작동 확인용 메시지
-    //    // player.speed = 0.5f;
-    //    yield return new WaitForSeconds(0.5f);
-    //    // player.speed = originalSpeed;
-    //}
+    ObstacleType GetRandomObstacleType()
 }
