3193ec639ad5fa9498f26422632d2dd59e17069f
minchangyun <yunchang0101@gmail.com>
2025-05-12 14:42:54 +0900

    feat - 획득?기능추가  #1
    플레이어와 콜럭터블 스크립트가 붙은
    게임오브젝트 충돌시 효과 발동하는 기능 추가

--- Assets/Scripts/03_Views/Collectable.cs
+++ Assets/Scripts/03_Views/Collectable.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class Collectable : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //충돌시 효과 적용 및 파괴처리, 과제가 어렵다면 코드 참고함
+    //아이템에 이 스크립트랑 아이템 모델 넣고 is trigger 켜두기
+    //
+    private ItemManager pool;
+    private void Awake()
     {
-        
+        pool = FindObjectOfType<ItemManager>();
     }
 
-    // Update is called once per frame
-    void Update()
+    // 플레이어가 트리거에 들어왔을 때 실행되는 함수
+    private void OnTriggerEnter2D(Collider2D other)//OnTriggerEnter2D는 2D Trigger 충돌이 일어났을 때 자동으로 호출해주는 유니티 내장 함수
     {
-        
+        if (other.CompareTag("Player"))//만약 충돌한 게임오브젝트의 태그가 플레이어라면
+        {
+            ItemModel item = GetComponent<ItemModel>();
+            if (item != null)
+            {
+                item.ApplyEffect();  // 아이템모델에 있는 효과 발동
+
+                pool.ReturnToPool(item.itemEnum, gameObject);//디스트로이, SetActiveFalse 대신 아이템 풀링 풀에 반환.
+            }
+        }
     }
 }
