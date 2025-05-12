a1b331d1dcc18ac8258a62c63eeb220aeb92a061
rmqrnjs6 <kkw201030@gmail.com>
2025-05-12 10:37:04 +0900

    feat : 최대 이동속도 고정 로직 추가 #1

--- Assets/Scripts/02_ViewModels/SpeedManager.cs
+++ Assets/Scripts/02_ViewModels/SpeedManager.cs
@@ -17,25 +17,16 @@ public class SpeedManager : MonoBehaviour
     private float speedIncreaseInterval = 0.5f; // 0.5초 마다 이동속도를 증가 시켜주는 speedIncreaseInterval 선언
     private float speedIncrement = 0.005f;
 
+    float MaxSpeed;
+
     private Coroutine speedCoroutine;
 
     void Start()
     {
         SetInitialSpeed();
+        SetMaxSpeed();
         speedCoroutine = StartCoroutine(IncreaseSpeedOverTime()); // 코루틴을 통하여 이동속도 증가와 이동속도의 최대 한도값 제어
     }
-
-    private IEnumerator IncreaseSpeedOverTime()
-    {
-        yield return new WaitForSeconds(speedIncreaseInterval);  // 게임이 시작된 후 speedIncreaseInterval(즉 0.5초)까지 대기하다 이동속도 값이 증가
-
-        while (true)
-        {
-            currentSpeed += speedIncrement; // 난이도 별로 이동속도 값(0.005)이 증가
-            yield return new WaitForSeconds(speedIncreaseInterval); // 다시 0.5초 대기 후 이동속도 값이 다시 증가
-        }
-    }
-
     private void SetInitialSpeed() // 난이도 별 이동속도 초기값 설정
     {
         switch (difficulty)
@@ -51,11 +42,45 @@ public class SpeedManager : MonoBehaviour
                 break;
         }
     }
-    public void StopIncreasingSpeed()
+
+    private void SetMaxSpeed() // 이동속도 상한선값 제어
+    {
+        switch (difficulty)
+        {
+            case Difficulty.Basic:
+                MaxSpeed = 5.0f;
+                break;
+            case Difficulty.Standard:
+                MaxSpeed = 6.0f;
+                break;
+            case Difficulty.Challenge:
+                MaxSpeed = 7.0f;
+                break;
+        }
+    }
+
+    private IEnumerator IncreaseSpeedOverTime()
+    {
+        yield return new WaitForSeconds(speedIncreaseInterval);  // 게임이 시작된 후 speedIncreaseInterval(즉 0.5초)까지 대기하다 이동속도 값이 증가
+
+        while (currentSpeed <= MaxSpeed) // currentSpeed(이동속도)가 증가하여 최대값인 MaxSpeed에 도달 할때 까지 
+        {
+            currentSpeed += speedIncrement; // 난이도 별로 이동속도 값(0.005)이 증가
+
+            if (currentSpeed >= MaxSpeed) // currentSpeed 가 증가를 반복하여 MaxSpeed 보다 넘게 됫을 경우
+                currentSpeed = MaxSpeed; // MaxSpeed보다 증가 되지 않도록 보정
+
+            yield return new WaitForSeconds(speedIncreaseInterval); // 다시 0.5초 대기 후 이동속도 값이 다시 증가
+        }
+        speedCoroutine = null; // 중복 실행 방지를 위해 값이 증가 된 후 null값 적용
+    }
+
+    public void StopIncreasingSpeed() // StageManager, MapData에서 사용
     {
         if (speedCoroutine != null) // speedCoroutine을 통하여 이동속도 값이 증가했을 경우 if 시작
         {
             StopCoroutine(speedCoroutine); // 이동속도가 증가된 후 Coroutine이 종료
+            speedCoroutine = null; // 중복 실행 방지를 위해 값이 증가 된 후 null값 적용
         }
     }
 }
